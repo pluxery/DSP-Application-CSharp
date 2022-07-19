5803d42ccabb8094b334ec4e7382c033e9f19715
pluxery <dranov7240@gmail.com>
2022-07-19 21:52:42 +1000

    add files

---
+++ WindowsFormsApplication2/AbstractGraphic.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsApp
+{
+    public  class AbstractGraphic:Form
+    {
+        protected bool localScaleMode=false;
+        protected bool gridMode =false;
+        protected bool markerMode=true;
+        public List<int> channelIndexes = new List<int>();
+        
+        public static Signal signal = Signal.GetInstance();
+        protected Chart curChart;
+        public List<Chart> charts = new List<Chart>();
+        
+        protected int X1;
+        protected int Y1;
+        protected int X2;
+        protected int Y2;
+        protected const int margin = 30;
+        
+        protected AbstractGraphic(){}
+        
+        public virtual void Init(int channelIndex)
+        {
+        }
+
+        protected virtual Chart CreateChart(int channelIndex)
+        {
+            return new Chart();
+            
+        }
+
+        protected virtual void Compute(int channelIndex){}
+
+        protected virtual void SetScale(){}
+        
+        protected double Min(in DataPointCollection points, int beginIndex, int endIndex)
+        {
+            double min = Double.MaxValue;
+            for (int i = beginIndex; i < endIndex; i++)
+            {
+                if (min > points[i].YValues[0])
+                    min = points[i].YValues[0];
+            }
+
+            return min;
+        }
+
+        protected double Max(in DataPointCollection points, int beginIndex, int endIndex)
+        {
+            double max = Double.MinValue;
+            for (int i = beginIndex; i < endIndex; i++)
+            {
+                if (max < points[i].YValues[0])
+                    max = points[i].YValues[0];
+            }
+
+            return max;
+        }
+
+    }
+}
---
+++ WindowsFormsApplication2/Factory.cs
@@ -0,0 +1,57 @@
+﻿using System.Drawing;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace WindowsApp
+{
+    public abstract class Factory
+    {
+        protected static Signal signal = Signal.GetInstance();
+        public abstract AbstractGraphic Create(int channelIndex);
+    }
+
+    public class FactoryFft : Factory
+    {
+        public override AbstractGraphic Create(int channelIndex)
+        {
+            if (signal.Fft == null)
+            {
+                var fft = new Fft(signal.MainForm);
+                fft.Init(channelIndex);
+                fft.Show();
+                return fft;
+            }
+
+            signal.Fft.Init(channelIndex);
+            signal.Fft.Show();
+            return signal.Fft;
+        }
+    }
+    public class FactoryStatistic : Factory
+    {
+        public override AbstractGraphic Create(int channelIndex)
+        {
+            var statistic = new Statistic(signal.MainForm);
+            statistic.Init(channelIndex);
+            statistic.Show();
+            return statistic;
+        }
+    }
+    public class FactoryOscillogram : Factory
+    {
+        public override AbstractGraphic Create(int channelIndex)
+        {
+            if (signal.Oscillogram == null)
+            {
+                var oscillogram = new Oscillogram(signal.MainForm);
+                oscillogram.Init(channelIndex);
+                oscillogram.Show();
+                return oscillogram;
+            }
+
+            signal.Oscillogram.Init(channelIndex);
+            signal.Oscillogram.Show();
+            return signal.Oscillogram;
+        }
+    }
+    
+}
